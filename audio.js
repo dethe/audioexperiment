7a6a9c33efba517baa2054282e0ed455e709937e
dethe <dethe@livingcode.org>
2012-12-09 22:01:16 -0800

    added left and right growls and ability to stop and start background

--- audio.js
+++ audio.js
@@ -20,48 +20,62 @@ mainVolume.connect(ctx.destination);
 function loadSound(soundFileName, loop, autoplay){
 	// Create an object with a sound source and a volume control.
 	var sound = {};
-	sound.source = ctx.createBufferSource();
-	sound.volume = ctx.createGainNode();
-
-	// Connect the sound source to the volume control.
-	sound.source.connect(sound.volume);
-	// Hook up the sound volume control to the main volume.
-	// sound.volume.connect(mainVolume);
-
-	// Make the sound source loop.
-	sound.source.loop = !!loop;
-
-	// Load a sound file using an ArrayBuffer XMLHttpRequest.
+	sound.isPlaying = false;
+	sound.loop = !!loop;
 	var request = new XMLHttpRequest();
 	request.open("GET", soundFileName, true);
 	request.responseType = "arraybuffer";
 	request.onload = function(e) {
 
 		// Create a buffer from the response ArrayBuffer.
-		var buffer = ctx.createBuffer(this.response, false);
 		sound.buffer = this.response;
 
 		// Make the sound source use the buffer and start playing it.
 		if (autoplay){
-			sound.play();
+			sound.play(0);
 		}
+	};
+	request.send();
+	sound.play = function(x){
+		sound.source = ctx.createBufferSource();
+		sound.volume = ctx.createGainNode();
+
+		// Connect the sound source to the volume control.
+		sound.source.connect(sound.volume);
+		// Hook up the sound volume control to the main volume.
+		// sound.volume.connect(mainVolume);
+
+		// Make the sound source loop.
+		sound.source.loop = this.loop;
+
+		// Load a sound file using an ArrayBuffer XMLHttpRequest.
 		sound.panner = ctx.createPanner();
 		// Instead of hooking up the volume to the main volume, hook it up to the panner.
 		sound.volume.connect(sound.panner);
 		// And hook up the panner to the main volume.
 		sound.panner.connect(mainVolume);
-	};
-	request.send();
-	sound.play = function(){
+		sound.panner.setPosition(x,0,0);
 		this.source.buffer = ctx.createBuffer(this.buffer, false);
-		this.source.noteOn(ctx.currentTime);
+		this.source.noteOn(0); // this has been changed to .start() in the spec
+		sound.isPlaying = true;
+	}
+	sound.stop = function(){
+		sound.source.noteOff(0); // this has been changed to .stop() in the spec
+		sound.isPlaying = false;
+	}
+	sound.toggle = function(){
+		if (this.isPlaying){
+			this.stop();
+		}else{
+			this.play(0);
+		}
 	}
 	return sound
 }
 
 
 var background = loadSound('horror_ambience.wav', true, true);
-var monster = loadSound('monster-1.wav');
+var monster = loadSound('monster-8.wav');
 var x = 0, y = 0, z = 0;
 // In the frame handler function, get the object's position.
 // And copy the position over to the sound of the object.
@@ -87,6 +101,18 @@ document.addEventListener('keydown', function(evt){
 		}else{
 			y -= 0.1;
 		}
+	}else if (evt.keyCode === 81){
+		// 'Q'
+		background.toggle();
+	}else if(evt.keyCode === 76){
+		// 'L'
+		monster.play(-10);
+	}else if(evt.keyCode === 82){
+		// R
+		monster.play(10);
+	}else{
+		console.log('evt.keyCode: %s', evt.keyCode);
+		return;
 	}
 	background.panner.setPosition(x,y,z);
 	console.log('x: %s, y: %s, z: %s', x, y, z);
